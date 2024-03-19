e6d23ebb25bf94328b85cb651c6657d63661092c
JP Whitaker <jpw@hawaii.edu>
2024-03-18 19:00:48 -1000

    started jp 3d text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import { useRef, useState, useEffect } from "react";
 import { EnergyChart } from "./(projects)/html/components/EnerygyChart.jsx"
 import { useControls } from "leva";
 import { useFrame } from "@react-three/fiber";
+import { Box, OrbitControls, Text3D } from "@react-three/drei";
 
 
 
@@ -28,22 +29,22 @@ export default function Home() {
   useEffect(() => {
     let animationFrameId;
     const precision = 0.01; // Define your precision threshold
-  
+
     const animate = () => {
       //how do i know how many fps this is?  0.005 is arbitrary, just chosen to make it a slow ramp up.
       const newBladeSpeed = MathUtils.lerp(bladeSpeedRef.current, targetSpeed, 0.005);
       // console.log({target: targetSpeed, new:newBladeSpeed});
       // setBladeSpeed(newBladeSpeed);
       bladeSpeedRef.current = newBladeSpeed
-  
+
       // Check if the difference is within the precision threshold
       if (Math.abs(newBladeSpeed - targetSpeed) > precision) {
         animationFrameId = requestAnimationFrame(animate);
       }
     };
-  
+
     animate();
-  
+
     return () => {
       cancelAnimationFrame(animationFrameId);
     };
@@ -53,10 +54,66 @@ export default function Home() {
   return (
     <main className="h-full bg-white">
 
+      <div className="mx-auto max-w-7xl h-96 bg-white-300 relative">
+        <Canvas>
+          <OrbitControls />
+          <Box args={[1.5, 1.5, 1.5]} position={[0, 0, 0]} />
+          {/* J */}
+          <Text3D font={'./golos.json'}
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.1}
+            height={0.01}
+            lineHeight={0.5}
+            letterSpacing={-0.06}
+            size={0.8}
+            position={[-0.3, -0.4, 0.8]}
+          >
+            J
+            <meshNormalMaterial />
+          </Text3D>
+          {/* P */}
+          <Text3D font={'./golos.json'}
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.1}
+            height={0.01}
+            lineHeight={0.5}
+            letterSpacing={-0.06}
+            size={0.8}
+            position={[0.8, -0.4, 0.3]} // Adjusted for right side of cube, considering Y rotation
+            rotation={[0, Math.PI / 2, 0]} // Rotated to face outwards correctly
+          >
+            P
+            <meshNormalMaterial />
+          </Text3D>
+          {/* W */}
+          {/* <Text3D font={'./golos.json'}
+            curveSegments={32}
+            bevelEnabled
+            bevelSize={0.04}
+            bevelThickness={0.1}
+            height={0.5}
+            lineHeight={0.5}
+            letterSpacing={-0.06}
+            size={0.8}
+            position={[-0.5, -0.4, -0.55]} // Adjusted for the top of the cube, considering Y rotation
+            rotation={[0, 0, 0]} // Rotated to face outwards correctly
+          >
+            W
+            <meshNormalMaterial />
+          </Text3D> */}
+        </Canvas>
+
+
+      </div>
+
       <div className="mx-auto max-w-7xl h-96 bg-blue-300 relative">
         {/* Floating div over the left side */}
         <div className="absolute top-0 left-0 h-full w-1/2 bg-white bg-opacity-50 z-10">
-          <EnergyChart bladeSpeedRef={bladeSpeedRef}/>
+          <EnergyChart bladeSpeedRef={bladeSpeedRef} />
         </div>
 
         {/* Existing canvas container */}
